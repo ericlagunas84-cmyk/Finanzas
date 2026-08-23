60fcc6e85ccead1e71a3b9aea2f6fc5ab768b7ea
ericlagunas84-cmyk <eric.lagunas84@gmail.com>
2026-08-22 18:16:47 -0600

    Refactor RecibosPage to simplify ScannerOCR usage

--- app/(app)/recibos/page.tsx
+++ app/(app)/recibos/page.tsx
@@ -1,5 +1,5 @@
-   import ScannerOCR from "@/components/recibos/ScannerOCR";
+import ScannerOCR from "@/components/recibos/ScannerOCR";
 
-   export default function RecibosPage() {
-     return <ScannerOCR onGuardado={() => {}} />;
-   }
+export default function RecibosPage() {
+  return <ScannerOCR />;
+}
